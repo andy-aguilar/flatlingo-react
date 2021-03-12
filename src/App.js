d573a8dfea8628f2fcaa3d2abcaa0587db8dc3a7
andy-aguilar <aaguil3@gmail.com>
2021-03-12 14:39:20 -0600

    Add icons format hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 import logo from './images/fastflash-logo.png'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className="header-nav">
-          <div className="logo-div">
-            <img className="header-logo" src={logo} alt="logo"/>
-            <h1 id="header-title">FastFlash</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <div className="App">
+          <header className="App-header">
+            <nav className="header-nav">
+              <div className="logo-div">
+                <img className="header-logo" src={logo} alt="logo"/>
+                <h1 id="header-title">FastFlash</h1>
+              </div>
+              <IconButton className="hamburger" aria-label="menu">
+                <MenuIcon fontSize="large"/>
+              </IconButton>
+            </nav>
+          </header>
+          <div id="main">
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            <h1>dumbshit</h1><br/>
+            
           </div>
-        </nav>
-      </header>
-      <main>
-
-      </main>
-    </div>
+        </div>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#778299',
+            main: '#556380',
+            dark: '#282c34',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#4398af',
+            main: '#61DAFB',
+            dark: '#80e1fb',
+            contrastText: '#000',
+        },
+      },
+})
+export default theme
