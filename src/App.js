49e8e31ad3d2eba230c9ed84833fe901fe3ce951
andy-aguilar <aaguil3@gmail.com>
2021-03-12 12:46:16 -0600

    Create header and logo, and make them responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import logo from './images/fastflash-logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <nav className="header-nav">
+          <div className="logo-div">
+            <img className="header-logo" src={logo} alt="logo"/>
+            <h1 id="header-title">FastFlash</h1>
+          </div>
+        </nav>
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
